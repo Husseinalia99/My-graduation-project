56853c7e2a23f179d21aad82b5b22ffa7fee778d
unknown <hussienalia99@gmail.com>
2023-11-17 09:26:48 -0700

    first commit

---
+++ Desktop/C#/test/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.h3 = new System.Windows.Forms.CheckBox();
+            this.a3 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.s1 = new System.Windows.Forms.CheckBox();
+            this.h1 = new System.Windows.Forms.CheckBox();
+            this.a1 = new System.Windows.Forms.CheckBox();
+            this.s2 = new System.Windows.Forms.CheckBox();
+            this.h2 = new System.Windows.Forms.CheckBox();
+            this.a2 = new System.Windows.Forms.CheckBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(759, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 381);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Controls.Add(this.h3);
+            this.groupBox2.Controls.Add(this.a3);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.s1);
+            this.groupBox2.Controls.Add(this.h1);
+            this.groupBox2.Controls.Add(this.a1);
+            this.groupBox2.Controls.Add(this.s2);
+            this.groupBox2.Controls.Add(this.h2);
+            this.groupBox2.Controls.Add(this.a2);
+            this.groupBox2.Location = new System.Drawing.Point(32, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(721, 381);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // h3
+            // 
+            this.h3.AutoSize = true;
+            this.h3.Location = new System.Drawing.Point(569, 124);
+            this.h3.Name = "h3";
+            this.h3.Size = new System.Drawing.Size(142, 17);
+            this.h3.TabIndex = 17;
+            this.h3.Text = "شديدة مع احساس بالحرقة";
+            this.h3.UseVisualStyleBackColor = true;
+            this.h3.CheckedChanged += new System.EventHandler(this.h3_CheckedChanged);
+            // 
+            // a3
+            // 
+            this.a3.AutoSize = true;
+            this.a3.Location = new System.Drawing.Point(569, 67);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(75, 17);
+            this.a3.TabIndex = 16;
+            this.a3.Text = "اكبر من 50";
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.CheckedChanged += new System.EventHandler(this.a3_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(166, 268);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(242, 96);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 96);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "تحليل الأعراض";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(4, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(257, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "هل تتعرض للشمس أو أجهزة التسمير كثير";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(49, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "شدة الحكة";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(55, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "العمر";
+            // 
+            // s1
+            // 
+            this.s1.AutoSize = true;
+            this.s1.Location = new System.Drawing.Point(277, 184);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(41, 17);
+            this.s1.TabIndex = 6;
+            this.s1.Text = "نعم";
+            this.s1.UseVisualStyleBackColor = true;
+            this.s1.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // h1
+            // 
+            this.h1.AutoSize = true;
+            this.h1.Location = new System.Drawing.Point(277, 121);
+            this.h1.Name = "h1";
+            this.h1.Size = new System.Drawing.Size(52, 17);
+            this.h1.TabIndex = 5;
+            this.h1.Text = "خفيفة";
+            this.h1.UseVisualStyleBackColor = true;
+            this.h1.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // a1
+            // 
+            this.a1.AutoSize = true;
+            this.a1.Location = new System.Drawing.Point(277, 64);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(111, 17);
+            this.a1.TabIndex = 4;
+            this.a1.Text = "اقل من عشرين عام";
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // s2
+            // 
+            this.s2.AutoSize = true;
+            this.s2.Location = new System.Drawing.Point(421, 187);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(32, 17);
+            this.s2.TabIndex = 2;
+            this.s2.Text = "لا";
+            this.s2.UseVisualStyleBackColor = true;
+            this.s2.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // h2
+            // 
+            this.h2.AutoSize = true;
+            this.h2.Location = new System.Drawing.Point(421, 124);
+            this.h2.Name = "h2";
+            this.h2.Size = new System.Drawing.Size(55, 17);
+            this.h2.TabIndex = 1;
+            this.h2.Text = "شديدة";
+            this.h2.UseVisualStyleBackColor = true;
+            this.h2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // a2
+            // 
+            this.a2.AutoSize = true;
+            this.a2.Location = new System.Drawing.Point(421, 67);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(88, 17);
+            this.a2.TabIndex = 0;
+            this.a2.Text = "من 30 إلى 50";
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(473, 268);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(242, 96);
+            this.richTextBox2.TabIndex = 18;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(249, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "النتيجة";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(586, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "النصائح";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CancelButton = this.button3;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(864, 436);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox s1;
+        private System.Windows.Forms.CheckBox h1;
+        private System.Windows.Forms.CheckBox a1;
+        private System.Windows.Forms.CheckBox s2;
+        private System.Windows.Forms.CheckBox h2;
+        private System.Windows.Forms.CheckBox a2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox h3;
+        private System.Windows.Forms.CheckBox a3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Desktop/C#/test/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        OpenFileDialog file = new OpenFileDialog();
+      
+        Bitmap image;
+        string filename;
+
+          string[] X = new string[] { "", "", "","" };
+
+
+
+        private void button2_Click_1(object sender, System.EventArgs e)
+        {
+         
+
+        }
+
+
+
+
+       
+
+      
+
+
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click_1(object sender, System.EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            if (X[0] == "")
+            {
+                MessageBox.Show("no data");
+
+
+            }
+            else
+            {
+
+                richTextBox1.Clear();
+                string fn = "C:/Users/windows.10/Desktop/data - Copy.txt";
+                int nx = 3;  // Number predictor variables
+                int nc = 3;  // Number classes
+                int N = 24;  // Number data items
+                string[][] data = LoadData(fn, N, nx + 1, ',');
+
+
+
+                int[][] jointCts = MatrixInt(nx, nc);
+                int[] yCts = new int[nc];
+
+
+
+                for (int i = 0; i < N; ++i)
+                {
+                    int y = int.Parse(data[i][nx]);
+                    ++yCts[y];
+                    for (int j = 0; j < nx; ++j)
+                    {
+                        if (data[i][j] == X[j])
+                            ++jointCts[j][y];
+                    }
+                }
+                // Laplacian smoothing
+                for (int i = 0; i < nx; ++i)
+                    for (int j = 0; j < nc; ++j)
+                        ++jointCts[i][j];
+
+
+
+
+                // Compute evidence terms
+                double[] eTerms = new double[nc];
+                for (int k = 0; k < nc; ++k)
+                {
+                    double v = 1.0;
+                    for (int j = 0; j < nx; ++j)
+                    {
+                        v *= (double)(jointCts[j][k]) / (yCts[k] + nx);
+                    }
+                    v *= (double)(yCts[k]) / N;
+                    eTerms[k] = v;
+                }
+
+
+
+                double evidence = 0.0;
+                for (int k = 0; k < nc; ++k)
+                    evidence += eTerms[k];
+                double[] probs = new double[nc];
+                for (int k = 0; k < nc; ++k)
+                    probs[k] = eTerms[k] / evidence;
+
+                // richTextBox1.AppendText("Probabilities: ");
+                // for (int k = 0; k < nc; ++k)
+                //   richTextBox1.AppendText( probs[k].ToString("F4") + "     ");
+                //richTextBox1.AppendText("\n");
+                richTextBox1.AppendText("Probabilities: % ");
+                for (int k = 0; k < nc; ++k)
+                    richTextBox1.AppendText(((probs[k] * 100)) + "     ");
+                richTextBox1.AppendText("\n");
+
+
+                int pc = ArgMax(probs);
+                richTextBox1.AppendText("Predicted class: ");
+                richTextBox1.AppendText(pc.ToString());
+                if (pc == 0)
+                {
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("eczema");
+                    richTextBox2.AppendText("الاكزيما: 1- لا تحك المنطقة المصابة 2- قم بترطيب بشرتك بانتظام 3- تجنب الاستحمام بمياه ساخنة جدا 4- لا تقم بالاستحمام لمدة تزيد عن عشر دقائق 5- ارتد ملابس قطنية 6- تجنب استخدام منتجات التقشير");
+                }
+
+                if (pc == 1)
+                {
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("cancer ");
+                    richTextBox2.AppendText("سرطان الجلد بأنواعه: لا تعرض الممنطقة المصابة لأشعة الشمس ولا لأجهزة التسمير 2- قم بتطبيق واقي شمسي في حال ستتعرض المنطقة المصابة للشمس 3- راجع طبيب الجلدية بأسرع وقت لأخذ خزعة وفحصها مجهريا والتأكد من الحالة المرضية ");
+                }
+                if (pc == 2)
+                {
+                    richTextBox1.AppendText("\n");
+                    richTextBox1.AppendText("psoriasis");
+                    richTextBox2.AppendText("الصدفية: 1- لا تحق المنطقة المصابة 2- خذ حماما يوميا لمدة لا تزيد عن عشر دقائق 3- قم بتغطية المنطقة المصابة عند النوم 4- عرض المنطقة لكمية قليلة من أشعة الشمس بفترة بعد الضهيرة أو الصباح الباكر 5- تجنب تناول منتجات الألبان");
+
+                }
+                for (int t=0;t<3;t++)
+                {
+                    X[t] = "";
+
+
+                }
+            }
+              
+            } 
+            static string[][] MatrixString(int rows, int cols)
+            {
+                string[][] result = new string[rows][];
+                for (int i = 0; i < rows; ++i)
+                    result[i] = new string[cols];
+                return result;
+            }
+            static int[][] MatrixInt(int rows, int cols)
+            {
+                int[][] result = new int[rows][];
+                for (int i = 0; i < rows; ++i)
+                    result[i] = new int[cols];
+                return result;
+            }
+            static string[][] LoadData(string fn, int rows,int cols, char delimit)
+            {
+                string[][] result = MatrixString(rows, cols);
+                FileStream ifs = new FileStream(fn, FileMode.Open);
+                StreamReader sr = new StreamReader(ifs);
+                string[] tokens = null;
+                string line = null;
+                int i = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    tokens = line.Split(delimit);
+                   
+                    for (int j = 0; j < cols; ++j)
+                        result[i][j] = tokens[j];
+                    
+                    ++i;
+                }
+                sr.Close(); ifs.Close();
+                return result;
+            }
+            static int ArgMax(double[] vector)
+            {
+                int result = 0;
+                double maxV = vector[0];
+                for (int i = 0; i < vector.Length; ++i)
+                {
+                    if (vector[i] > maxV)
+                    {
+                        maxV = vector[i];
+                        result = i;
+                    }
+                }
+                return result;
+            }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            X[0]="b";
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+         X[0]="a";
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+             X[1]="B";
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+             X[1]="A";
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+             X[2]="e";
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+             X[2]="d";
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+             X[2]="D";
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+             X[2]="d";
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a3_CheckedChanged(object sender, EventArgs e)
+        {
+            X[0] = "c";
+        }
+
+        private void h3_CheckedChanged(object sender, EventArgs e)
+        {
+            X[0] = "C";
+        }
+    }
+
+
+
+
+
+
+    }
+
+
+
+
+        
+    
+
